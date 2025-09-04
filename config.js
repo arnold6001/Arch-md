d314fa2e6fde9c2cc2486a58c61d3587d0eb1832
Goodness Tech <138625969+GoodnessObilom@users.noreply.github.com>
2025-09-04 19:33:41 +0100

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 /*
- * Base By Ednut
+ * Base By ɢᴏᴏᴅɴᴇꜱꜱ ᴛᴇᴄʜ 
  * Created On 11/10/2024
- * Contact Me on wa.me/2348102487241
+ * Contact Me on wa.me/2348065623101
 */
 
 const chalk = require("chalk");
@@ -10,20 +10,20 @@ require("dotenv").config();
 
 //==================[ BOT SETTINGS ]==================\\
 global.prefix      = (process.env.PREFIX || ".").trim().split(/\s+/); // Supports multiple or single prefix
-global.owner       = process.env.OWNER_NUMBER || "2347036214381";     // Owner number (for multiple, use comma-separated in your logic)
+global.owner       = process.env.OWNER_NUMBER || "2348065623101";     // Owner number (for multiple, use comma-separated in your logic)
 global.sudo        = process.env.SUDO || "";                          // Sudo users (string or logic processed array)
-global.ownername   = process.env.OWNER_NAME || "Ednut";              // Owner name
+global.ownername   = process.env.OWNER_NAME || "Goodness Tech";              // Owner name
 global.botname     = process.env.BOT_NAME || "null";                 // Bot name
-global.author      = process.env.AUTHOR || "Ednut";                  // Sticker author
+global.author      = process.env.AUTHOR || "Goodness Tech";                  // Sticker author
 global.packname    = process.env.PACK_NAME || "Arch Md ²⁵";          // Sticker pack name
 global.session     = process.env.SESSION_ID || "";                   // Session ID
 global.timezone    = process.env.TIME_ZONE || "Africa/Lagos";        // Default timezone
 global.simbol      = process.env.SYMBOL || "♘";                      // Menu icon/symbol
 global.thumb       = process.env.THUMB || "https://files.catbox.moe/bfzvlr.jpg,https://files.catbox.moe/n7wo5h.jpg,https://files.catbox.moe/sidp03.jpg,https://files.catbox.moe/yvb0et.jpg,https://files.catbox.moe/vu2qgl.jpg,https://files.catbox.moe/5rfbw9.jpg,https://files.catbox.moe/5hpljo.jpg,https://files.catbox.moe/o5hve8.jpg,https://files.catbox.moe/7383y9.jpg";
-global.footer      = process.env.FOOTER || "";                       // Footer for captions
-global.warn        = process.env.WARN || "4";                        // Warning threshold
+global.footer      = process.env.FOOTER || "*ᴘᴏᴡᴇʀᴇᴅ ʙʏ ɢᴏᴏᴅɴᴇꜱꜱ ᴛᴇᴄʜ*";                       // Footer for captions
+global.warn        = process.env.WARN || "3";                        // Warning threshold
 global.menutype    = process.env.MENU_TYPE || "";                    // Menu style
-global.scan        = "https://arch.ednut.xyz/";                      // Scan link
+global.scan        = "https://arch-md-site.vercel.app/";                      // Scan link
 
 //==================[ BOT MESSAGES ]==================\\
 global.msg = {
